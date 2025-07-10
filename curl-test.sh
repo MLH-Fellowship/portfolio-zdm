2df59165d09c559caae430622c7fe0cb7f5c0c2e
zd <zidanni.clerigo@gmail.com>
2025-07-09 22:05:31 -0400

    curl test fix

--- curl-test.sh
+++ curl-test.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-BASE_URL="http://localhost:5000"
-
 RANDOM_NUM=$((RANDOM))
 TEST_NAME="test User $RANDOM_NUM"
-TEST_EMAIL="testuser$RANDOM_NUM@example.com"
+TEST_EMAIL="testuser$RANDOM_NUM@test.com"
 TEST_CONTENT="test"
 
-curl -X POST "$BASE_URL/api/timeline_post" \
-  -d "name=$TEST_NAME" \
-  -d "email=$TEST_EMAIL" \
-  -d "content=$TEST_CONTENT" \
+curl -X POST http://localhost:5000/api/timeline_post -d "name=$TEST_NAME&email=$TEST_EMAIL&content=$TEST_CONTENT"
 
-curl -s "$BASE_URL/api/timeline_post"
+curl "http://localhost:5000/api/timeline_post"
