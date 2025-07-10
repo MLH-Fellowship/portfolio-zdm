9e5a4dcf78e5db47914dabf1909a4e1197de92b2
Deeptanshu-sankhwar <deep.shanky@gmail.com>
2025-07-10 17:41:44 +0700

    push mysqldatabase changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-
+from peewee import MySQLDatabase
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 # Initialize Jinja2 environment
@@ -13,6 +13,16 @@ env = Environment(
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(
+    os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306,
+)
+
+print(mydb)
 
 NAV_ITEMS = [
     {'name': 'Zidanni', 'url': '/', 'route': 'index'},
