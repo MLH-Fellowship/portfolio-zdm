a1625cddb84df0cdf61e3545b19f6765f9f19a33
Fahim H <fahimhussain21@gmail.com>
2025-07-17 21:37:46 -0400

    Add passing tests for db insertion and reads

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
-from peewee import MySQLDatabase, Model, CharField, TextField, DateTimeField
+from peewee import MySQLDatabase, SqliteDatabase, Model, CharField, TextField, DateTimeField
 from jinja2 import Environment, PackageLoader, select_autoescape
 from playhouse.shortcuts import model_to_dict
 from datetime import datetime
@@ -16,13 +16,17 @@ env = Environment(
 load_dotenv()
 app = Flask(__name__)
 
-mydb = MySQLDatabase(
-    os.getenv("MYSQL_DATABASE"),
-    user=os.getenv("MYSQL_USER"),
-    password=os.getenv("MYSQL_PASSWORD"),
-    host=os.getenv("MYSQL_HOST"),
-    port=3306,
-)
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase("file:memory?mode=memory&cache=shared", uri=True)
+else:
+    mydb = MySQLDatabase(
+        os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306,
+    )
 
 class TimelinePost(Model):
     name = CharField()
