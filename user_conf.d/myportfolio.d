d5576d65a54258273715f976bae7a9198573c479
root <zidanni.clerigo@gmail.com>
2025-07-25 16:58:34 +0000

    fix

--- user_conf.d/myportfolio.d
+++ user_conf.d/myportfolio.d
@@ -12,13 +12,13 @@ server {
    server_name zidannimlh.duckdns.org;
 
    location / {
-       proxy_pass http://myportfolio:5000/;
+       proxy_pass http://myportfolio:5000;
    }
 
    # Load the certificate files.
-   ssl_certificate /etc/letsencrypt/live/myportfolio/fullchain.pem;
+   ssl_certificate /etc/letsencrypt/live/zidannimlh.duckdns.org/fullchain.pem;
    ssl_certificate_key
-       /etc/letsencrypt/live/myportfolio/privkey.pem;
+       /etc/letsencrypt/live/zidannimlh.duckdns.org/privkey.pem;
    ssl_trusted_certificate
-       /etc/letsencrypt/live/myportfolio/chain.pem;
+       /etc/letsencrypt/live/zidannimlh.duckdns.org/chain.pem;
 }
