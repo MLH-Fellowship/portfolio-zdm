50a712ebda13201985ba4fe267ec3bb245007669
root <zidanni.clerigo@gmail.com>
2025-07-26 01:21:30 +0000

    fi

--- user_conf.d/myportfolio.d
+++ user_conf.d/myportfolio.d
@@ -1,24 +1,25 @@
 server {
-   listen 80;
-   server_name zidannimlh.duckdns.org;
-
-   if ($host = zidannimlh.duckdns.org) {
-       return 301 https://$host$request_uri;
-   }
-}
-
-server {
    listen 443 ssl;
    server_name zidannimlh.duckdns.org;
 
    location / {
        proxy_pass http://myportfolio:5000;
+       proxy_set_header Host $host;
+       proxy_set_header X-Real-IP $remote_addr;
+       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+       proxy_set_header X-Forwarded-Proto $scheme;
    }
 
-   # Load the certificate files.
+   # SSL configuration
    ssl_certificate /etc/letsencrypt/live/zidannimlh.duckdns.org/fullchain.pem;
-   ssl_certificate_key
-       /etc/letsencrypt/live/zidannimlh.duckdns.org/privkey.pem;
-   ssl_trusted_certificate
-       /etc/letsencrypt/live/zidannimlh.duckdns.org/chain.pem;
+   ssl_certificate_key /etc/letsencrypt/live/zidannimlh.duckdns.org/privkey.pem;
+   ssl_trusted_certificate /etc/letsencrypt/live/zidannimlh.duckdns.org/chain.pem;
+   
+   # SSL settings
+   ssl_session_cache shared:le_nginx_SSL:10m;
+   ssl_session_timeout 1440m;
+   ssl_session_tickets off;
+   ssl_protocols TLSv1.2 TLSv1.3;
+   ssl_prefer_server_ciphers off;
+   ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
 }
